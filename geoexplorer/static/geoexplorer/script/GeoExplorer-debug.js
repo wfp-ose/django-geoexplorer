7a338098e3e3f1b3ae9e545cdab3f6fcedd359f6
karakostis <karakostis.dimitris@gmail.com>
2017-07-18 08:54:19 +0200

    changes in urls

--- geoexplorer/static/geoexplorer/script/GeoExplorer-debug.js
+++ geoexplorer/static/geoexplorer/script/GeoExplorer-debug.js
@@ -59377,7 +59377,7 @@ gxp.PointSymbolizer = Ext.extend(Ext.Panel, {
                 {display: this.graphicXText, display:"Earthquake", value: "https://staging.geonode.wfp.org/uploaded/geoserver_icons/standard/Emergency/earthquake.png", mark: false},
                 {display: this.graphicXText, display:"Port", value: "https://staging.geonode.wfp.org/uploaded/geoserver_icons/Global_Layers/Port.png", mark: false},
                 {display: this.graphicXText, display:"Refugee Camp", value: "https://staging.geonode.wfp.org/uploaded/geoserver_icons/Global_Layers/opweb_refugee_camp.png", mark: false}, 
-                {display: this.graphicXText, display:"Warehouse", value: "https://staging.geonode.wfp.org/uploaded/geoserver_icons/standard/WFP_Corporate_Icons/Schoolfeeding_neg.png", mark: false},
+                {display: this.graphicXText, display:"Warehouse", value: "https://staging.geonode.wfp.org/uploaded/geoserver_icons/standard/WFP_Corporate_Icons/Warehouse_neg.png", mark: false},
                 {display: this.graphicCircleText, value: "circle", mark: true},
                 {display: this.graphicCrossText, value: "cross", mark: true},
                 {display: this.graphicSquareText, value: "square", mark: true},
